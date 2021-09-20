c3ddfd272f5ebcc4db141c6bf39bb7994a5bc504
Serhiy Stepanenko <serhiy.stepanenko@newfireglobal.com>
2021-09-20 17:26:01 +0300

    review

--- essentials/iterator.py
+++ essentials/iterator.py
@@ -5,7 +5,7 @@ class inclusive_range:
         numargs = len(args)
         self._start = 0
         self._step = 1
-        
+
         if numargs < 1:
             raise TypeError(f'expected at least 1 argument, got {numargs}')
         elif numargs == 1:
@@ -14,10 +14,11 @@ class inclusive_range:
             (self._start, self._stop) = args
         elif numargs == 3:
             (self._start, self._stop, self._step) = args
-        else: raise TypeError(f'expected at most 3 arguments, got {numargs}')
+        else:
+            raise TypeError(f'expected at most 3 arguments, got {numargs}')
 
         self._next = self._start
-    
+
     def __iter__(self):
         return self
 
@@ -29,9 +30,20 @@ class inclusive_range:
             self._next += self._step
             return _r
 
+
 def main():
-    for n in inclusive_range(25):
+    for n in inclusive_range(20):
         print(n, end=' ')
     print()
 
-if __name__ == '__main__': main()
+    for n in inclusive_range(5, 20):
+        print(n, end=' ')
+    print()
+
+    for n in inclusive_range(7, 30, 2):
+        print(n, end=' ')
+    print()
+
+
+if __name__ == '__main__':
+    main()
