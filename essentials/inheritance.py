fb49c3d41f2bab5f8f0a717887e1560d55946df7
Serhiy Stepanenko <serhiy.stepanenko@newfireglobal.com>
2021-09-26 13:11:16 +0300

    reviewed

---
+++ essentials/inheritance.py
@@ -0,0 +1,68 @@
+
+class Animal:
+    def __init__(self, **kwargs):
+        if 'type' in kwargs:
+            self._type = kwargs['type']
+        if 'name' in kwargs:
+            self._name = kwargs['name']
+        if 'sound' in kwargs:
+            self._sound = kwargs['sound']
+
+    def type(self, t=None):
+        if t:
+            self._type = t
+        try:
+            return self._type
+        except AttributeError:
+            return None
+
+    def name(self, n=None):
+        if n:
+            self._name = n
+        try:
+            return self._name
+        except AttributeError:
+            return None
+
+    def sound(self, s=None):
+        if s:
+            self._sound = s
+        try:
+            return self._sound
+        except AttributeError:
+            return None
+
+
+class Duck(Animal):
+    def __init__(self, **kwargs):
+        self._type = 'duck'
+        if 'type' in kwargs:
+            del kwargs['type']
+        super().__init__(**kwargs)
+
+
+class Kitten(Animal):
+    def __init__(self, **kwargs):
+        self._type = 'kitten'
+        if 'type' in kwargs:
+            del kwargs['type']
+        super().__init__(**kwargs)
+
+
+def print_animal(o):
+    if not isinstance(o, Animal):
+        raise TypeError('print_animal(): requires an Animal')
+    print(f'The {o.type()} is named "{o.name()}" and says "{o.sound()}".')
+
+
+def main():
+    a0 = Duck(name='donald', sound='quack')
+    a1 = Kitten(name='fluffy', sound='rwar')
+    a2 = Kitten(type='dragon', name='tor', sound='wooo')
+    print_animal(a0)
+    print_animal(a1)
+    print_animal(a2)
+
+
+if __name__ == '__main__':
+    main()
